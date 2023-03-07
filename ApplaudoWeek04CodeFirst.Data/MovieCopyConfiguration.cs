6bd0a4b72c25bc1e8a20f80f4348fd14f77b5384
Luis <luis.w.rivera.c@gmail.com>
2023-03-07 16:56:04 -0500

    add movie copy configuration

---
+++ ApplaudoWeek04CodeFirst.Data/MovieCopyConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ApplaudoWeek04CodeFirst.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplaudoWeek04CodeFirst.Data
+{
+    public class MovieCopyConfiguration : IEntityTypeConfiguration<MovieCopy>
+    {
+        public void Configure(EntityTypeBuilder<MovieCopy> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasData(
+                new MovieCopy { Id=1, MovieId=1, Available=true },
+                new MovieCopy { Id=2, MovieId=1, Available=true },
+                new MovieCopy { Id=3, MovieId=1, Available=true },
+                new MovieCopy { Id=4, MovieId=2, Available=false },
+                new MovieCopy { Id=5, MovieId=2, Available=true },
+                new MovieCopy { Id=6, MovieId=3, Available=false },
+                new MovieCopy { Id=7, MovieId=3, Available=true },
+                new MovieCopy { Id=8, MovieId=3, Available=true }
+            );
+        }
+    }
+}
