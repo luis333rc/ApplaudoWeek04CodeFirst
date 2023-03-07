1486ae6bee05049b4062b46c96b9a31dea1a9a95
Luis <luis.w.rivera.c@gmail.com>
2023-03-07 15:35:53 -0500

    add movies rental dbcontext

---
+++ ApplaudoWeek04CodeFirst.Data/MoviesRentalDbContext.cs
@@ -0,0 +1,50 @@
+﻿using ApplaudoWeek04CodeFirst.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplaudoWeek04CodeFirst.Data
+{
+    public  class MoviesRentalDbContext :DbContext
+    {
+        public class MoviesDbContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-PH8N518;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Database=MovieRental")
+                //.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name })
+                //.EnableSensitiveDataLogging()
+                ;
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                
+                modelBuilder.Entity<MovieTag>()
+                    .HasKey(a => new { a.MovieId, a.TagId });
+
+                modelBuilder.Entity<MovieLike>()
+                    .HasKey(a => new { a.MovieId, a.CustomerId });
+
+            }
+
+            DbSet<Customer> Customers { get; set; }
+
+            DbSet<Movie> Movies { get; set; }
+
+            DbSet<MovieCopy> MovieCopies { get; set; }
+
+            DbSet<MovieTag> MovieTags { get; set; }
+
+            DbSet<Rental> Rentals { get; set; }
+
+            DbSet<Tag> Tags { get; set; }
+
+            DbSet<MovieLike> MovieLikes { get; set; }
+
+        }
+    }
+}
